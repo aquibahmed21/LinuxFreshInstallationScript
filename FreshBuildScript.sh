3152d72387e642f63cdc41dbb768c79ed7e7dd16
Aquib Ahmed <aquibahmed21@gmail.com>
2021-09-23 21:37:45 +0530

    Added clean repository

---
+++ FreshBuildScript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+repositories="\
+deb http://deb.debian.org/debian bullseye main contrib non-free
+deb-src http://deb.debian.org/debian bullseye main contrib non-free
+
+deb http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+deb-src http://deb.debian.org/debian-security/ bullseye-security main contrib non-free
+
+deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
+
+deb http://deb.debian.org/debian bullseye-backports main contrib non-free
+deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free
+"
+
+sudo truncate -s0 /etc/apt/sources.list &&
+echo "$repositories" > /etc/apt/sources.list
