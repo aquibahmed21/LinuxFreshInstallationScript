8510aa8fadefdb4cc08ccb5e21bda11ba11d42ea
Aquib Ahmed <aquibahmed21@gmail.com>
2021-09-27 13:03:09 +0530

    added conf files, removing bloatwares

--- FreshBuildScript.sh
+++ FreshBuildScript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# List of repositories
 repositories="\
 deb http://deb.debian.org/debian bullseye main contrib non-free
 deb-src http://deb.debian.org/debian bullseye main contrib non-free
@@ -14,7 +15,57 @@ deb http://deb.debian.org/debian bullseye-backports main contrib non-free
 deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free
 "
 
+# cleans sources file and inserts values from repositories
 sudo truncate -s0 /etc/apt/sources.list &&
 echo "$repositories" > /etc/apt/sources.list
 
-sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y full-upgrade && sudo apt-get -y install --fix-missing --fix-broken && sudo apt-get -y clean && sudo apt-get -y autoremove
+# updates and cleans the OS
+sudo apt-get -y update &&
+sudo apt-get -y upgrade &&
+sudo apt-get -y dist-upgrade &&
+sudo apt-get -y full-upgrade &&
+sudo apt-get -y install --fix-missing --fix-broken &&
+sudo apt-get -y clean &&
+sudo apt-get -y autoremove
+
+# Sets the current time and timezone
+sudo timedatectl set-timezone Asia/Kolkata
+
+# Removes the typing of username at start-up
+sudo bash -c 'echo "[SeatDefaults]" >> /usr/share/lightdm/lightdm.conf.d/01_my.conf' &&
+sudo bash -c 'echo "greeter-hide-users=false" >> /usr/share/lightdm/lightdm.conf.d/01_my.conf'
+
+# Theming Terminator
+cat terminatorUI.txt >> ~/.bashrc
+
+# Removing bloatwares
+sudo apt-get -y purge --auto-remove empathy brasero sound-juicer totem pidgin firefox-esr gimp imagemagick-* hexchat firefox thunderbird chromium remmina #synaptic*
+
+rm -rf ~/.mozilla
+rm -rf ~/.config/chromium
+rm -rf ~/.cache/chromium
+sudo rm -rf /etc/chromium
+
+# Removing games
+sudo apt-get -y purge --auto-remove gnome-2048 aisleriot atomix gnome-chess five-or-more hitori iagno gnome-klotski lightsoff gnome-mahjongg gnome-mines gnome-nibbles quadrapassel four-in-a-row gnome-robots gnome-sudoku swell-foop tali gnome-taquin gnome-tetravex
+
+# Downloads
+
+# Essentials
+# sudo apt-get -y install firmware-realtek
+# sudo apt-get -y install firmware-linux-nonfree
+# sudo apt-get -y install software-properties-common apt-transport-https wget
+
+# Installing Chrome
+# wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# sudo apt install -y ./google-chrome-stable_current_amd64.deb
+
+# Installing VS-Code
+# wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+# sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+# sudo apt-get -y update
+# sudo apt-get install -y code
+
+
+
+echo "success"
